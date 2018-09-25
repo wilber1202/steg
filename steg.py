9f37a527e674b5eb4e2865fefa203c9f5bc46e97
wilber1202 <wilber1202@gmail.com>
2018-09-25 16:39:10 +0800

    auto steg

---
+++ steg.py
@@ -0,0 +1,31 @@
+import sys
+import subprocess
+import os
+			 
+if (1==len(sys.argv)):
+	print("Miss file...")
+	sys.exit(0)
+	
+filename=sys.argv[1]
+
+steg_tools=[("", "strings -a", "> result_strings.txt"), 
+		   			("", "zsteg", "> result_zsteg.txt"), 
+		   			("", "file_type", "> result_filetype.txt"),
+		   			("png", "pngcheck -vf", "> result_pngcheck.txt"),
+		   			("", "outguess -r", "result_outguess.txt"),
+		   			("", "foremost", ""),
+		   			("gif", "identify -format \"%T\"", "> result_identify.txt"),
+		   			("", "exiftool", "> result_exiftool.txt"),
+		   			("", "binwalk -e", ""),
+		   			("", "file_modify", "")]
+	
+for i in range(0, len(steg_tools)):
+	if ((0 != len(steg_tools[i][0])) and (False == filename.endswith(steg_tools[i][0]))):
+		continue
+	cmd = steg_tools[i][1] + " " + filename + " " + steg_tools[i][2]
+	print(cmd)
+	result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	result.wait()
+	lines = result.stdout.readlines()
+	for line in lines:
+		print("%s" % line)
