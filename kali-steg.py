97e54185594cde4eddaef2d35b7cf70f0eca15f1
wilber1202 <wilber1202@gmail.com>
2021-02-19 10:46:29 +0800

    Update kali-steg.py
    add stepic & convert

--- kali-steg.py
+++ kali-steg.py
@@ -1,31 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys
 import subprocess
 import os
 			 
-if (1==len(sys.argv)):
+if (1 == len(sys.argv)):
 	print("Miss file...")
 	sys.exit(0)
 	
-filename=sys.argv[1]
+filename	= sys.argv[1]
 
-steg_tools=[("", "strings -a", "> result_strings.txt"), 
-		   			("", "zsteg", "> result_zsteg.txt"), 
-		   			("", "file_type", "> result_filetype.txt"),
-		   			("png", "pngcheck -vf", "> result_pngcheck.txt"),
-		   			("", "outguess -r", "result_outguess.txt"),
-		   			("", "foremost", ""),
-		   			("gif", "identify -format \"%T\"", "> result_identify.txt"),
-		   			("", "exiftool", "> result_exiftool.txt"),
-		   			("", "binwalk -e", ""),
-		   			("", "file_modify", ""),
-		   			("", "python /usr/bin/lsb.py extract", "result_lsb.txt 123456")]
-	
+steg_tools	= [("", "strings -a", "> result_strings.txt"), 
+		   	   ("", "remove0x00", ""),
+		   	   ("", "zsteg", "> result_zsteg.txt"), 
+		   	   ("", "file_type", "> result_filetype.txt"),
+		   	   ("png", "pngcheck -vf", "> result_pngcheck.txt"),
+		   	   ("", "outguess -r", "result_outguess.txt"),
+		   	   ("", "foremost", ""),
+		   	   ("gif", "identify -format \"%T\"", "> result_identify.txt"),
+		   	   ("", "exiftool", "> result_exiftool.txt"),
+		   	   ("", "binwalk -e", ""),
+		   	   ("", "file_modify", ""),
+		   	   ("", "python /usr/bin/lsb.py extract", "result_lsb.txt 123456"),
+		   	   ("", "stepic -d -i", "> result_stepic.txt"),
+		   	   ("gif", "convert", "newimage.png")]
+
 for i in range(0, len(steg_tools)):
 	if ((0 != len(steg_tools[i][0])) and (False == filename.endswith(steg_tools[i][0]))):
 		continue
 	cmd = steg_tools[i][1] + " " + filename + " " + steg_tools[i][2]
 	print(cmd)
-	result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	result = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 	result.wait()
 	lines = result.stdout.readlines()
 	for line in lines:
